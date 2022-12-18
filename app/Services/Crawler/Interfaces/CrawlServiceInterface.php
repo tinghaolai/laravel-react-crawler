822cb0777a48928f9bd3623e29541445bae7cada
tinghaolai <analth355@gmail.com>
2022-12-18 12:39:01 +0800

    Crawl service specify interface functions

--- app/Services/Crawler/Interfaces/CrawlServiceInterface.php
+++ app/Services/Crawler/Interfaces/CrawlServiceInterface.php
@@ -2,6 +2,13 @@
 
 namespace App\Services\Crawler\Interfaces;
 
+use App\Services\ServiceResult;
+
 interface CrawlServiceInterface
 {
+    public function search(int $perPage, array $conditions): ServiceResult;
+
+    public function craw(string $url): ServiceResult;
+
+    public function get(int $id): ServiceResult;
 }
